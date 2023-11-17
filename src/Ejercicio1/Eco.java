266a56ea7cd52a40c6d383244d37c4dfe6b1a21d
Danielip1705 <daniel.iglesias@iesnervion.es>
2023-11-17 14:41:47 +0100

    Ejercicio

---
+++ src/Ejercicio1/Eco.java
@@ -0,0 +1,13 @@
+package Ejercicio1;
+
+public class Eco {
+	public static void Eco
+	int n; {
+		for(int i=1;i<n;i++) {
+			System.out.println("Eco...");
+		}
+		
+	}
+	
+}
+
---
+++ src/Ejercicio1/Ecomain.java
@@ -0,0 +1,16 @@
+package Ejercicio1;
+
+import java.util.Scanner;
+import Ejercicio1.Eco;
+public class Ecomain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n;
+		Scanner sc = new Scanner(System.in);
+		sc.nextInt();
+		
+		sc.close();
+	}
+
+}
